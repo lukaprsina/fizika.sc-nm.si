8a6743895ed448758b9e570c7bef8e184e3319bb
Luka Pršina <luk.prsina@gmail.com>
2022-05-20 23:30:45 +0200

    before parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 use headless_chrome::{Browser, LaunchOptionsBuilder, Tab};
-use std::{
-    error::Error,
-    fs::File,
-    io::{BufRead, BufReader},
-    path::Path,
-    sync::Arc,
-    thread::sleep,
-    time::Duration,
-};
+use std::{error::Error, sync::Arc, thread::sleep, time::Duration};
 use url::Url;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -20,32 +12,59 @@ fn main() -> Result<(), Box<dyn Error>> {
     let lines = get_links(&tab)?;
     let url = Url::parse(&tab.get_url())?;
 
-    /* for line in lines {
+    for line in lines {
         let new_address = url.join(&line)?;
-        println!("{}", new_address);
         tab.navigate_to(&new_address.to_string())?;
         tab.wait_until_navigated()?;
         process_tab(&tab)?;
         sleep(Duration::from_secs(1));
-    } */
+    }
 
     Ok(())
 }
 
 fn get_links(tab: &Arc<Tab>) -> Result<Vec<String>, Box<dyn Error>> {
-    let links = Vec::new();
+    let mut links = Vec::new();
     let elements = tab.find_elements("body > div a")?;
     for element in elements {
         let attributes = element.get_attributes()?;
-        if let Some(attributes) = attributes {
-            if let Some(on_click) = attributes.get("onclick") {
-                println!("{}", on_click);
-            }
-        }
+        let attributes = attributes.unwrap();
+        let on_click = attributes.get("onclick").unwrap();
+        let start = "window.open(\'".len();
+        let end = on_click
+            .chars()
+            .skip(start)
+            .position(|c| c == '\'')
+            .unwrap();
+        let tab_str = on_click.chars().skip(start).take(end).collect::<String>();
+        links.push(tab_str);
     }
     Ok(links)
 }
 
-fn process_tab(_tab: &Arc<Tab>) -> Result<(), Box<dyn Error>> {
-    Ok(())
+#[derive(Default)]
+struct Chapter {
+    title: String,
+    exercises: Vec<Exercise>,
+}
+
+enum MediaType {
+    Image,
+    Video,
+    Audio,
+    Hint,
+    Solution,
+    Button,
+}
+
+struct Exercise {
+    content: Vec<MediaType>,
+}
+
+fn process_tab(tab: &Arc<Tab>) -> Result<Chapter, Box<dyn Error>> {
+    let mut a = Chapter::default();
+    a.title = tab.get_title()?;
+    let items = tab.find_elements("#container div")?;
+    println!("{:?}", &items);
+    Ok(a)
 }
